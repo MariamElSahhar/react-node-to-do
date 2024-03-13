272596f83e6376195a90aed8f8a2b5d5936e0d4c
MariamElSahhar <me1693@nyu.edu>
2024-03-13 11:30:16 +0400

    task controller done

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -30,13 +30,19 @@ exports.createTask = (req, res) => {
 		});
 	} else {
 		const newTask = req.body;
-		if (!newTask.description) {
+		if (!("status" in newTask) || !("description" in newTask)) {
 			res.status(400).json({
 				status: "error",
 				message: "incomplete task data",
 			});
 		} else {
-			data[profileIndex].tasks["todo"].push(newTask.description);
+			if (data[profileIndex].tasks.length === 0) newTask.id = 0;
+			else
+				newTask.id =
+					data[profileIndex].tasks[
+						data[profileIndex].tasks.length - 1
+					].id + 1;
+			data[profileIndex].tasks.push(newTask);
 			fs.writeFile(filepath, JSON.stringify(data), (err) => {
 				if (err)
 					res.status(500).json({
@@ -46,7 +52,7 @@ exports.createTask = (req, res) => {
 				else
 					res.status(201).json({
 						status: "success",
-						results: data[profileIndex].tasks["todo"].length,
+						results: data[profileIndex].tasks.length,
 						data: { newTask },
 					});
 			});
@@ -55,22 +61,112 @@ exports.createTask = (req, res) => {
 };
 
 exports.getTaskByID = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "route not implemented",
-	});
+	const profileID = req.params.profileID * 1;
+	const profileIndex = data.findIndex((profile) => profile.id === profileID);
+	if (profileIndex === -1) {
+		res.status(404).json({
+			status: "error",
+			message: "profile not found",
+		});
+	} else {
+		const taskID = req.params.id * 1;
+		const taskIndex = data[profileIndex].tasks.findIndex(
+			(task) => task.id === taskID
+		);
+		if (taskIndex === -1)
+			res.status(404).json({
+				status: "error",
+				message: "task not found",
+			});
+		else
+		{
+			const taskSearch = data[profileIndex].tasks[taskIndex]
+			res.status(200).json({
+				status: "success",
+				message: "task found",
+				data: {
+					taskSearch
+				},
+			});}
+	}
 };
 
 exports.deleteTask = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "route not implemented",
-	});
+	const profileID = req.params.profileID * 1;
+	const profileIndex = data.findIndex((profile) => profile.id === profileID);
+	if (profileIndex === -1) {
+		res.status(404).json({
+			status: "error",
+			message: "profile not found",
+		});
+	} else {
+		const taskID = req.params.id * 1;
+		const taskSearch = data[profileIndex].tasks.findIndex(
+			(task) => task.id === taskID
+		);
+		if (taskSearch === -1) {
+			res.status(404).json({
+				status: "error",
+				message: "task not found",
+			});
+		} else {
+			data[profileIndex].tasks = data[profileIndex].tasks.filter(
+				(task) => task.id !== taskID
+			);
+			fs.writeFile(filepath, JSON.stringify(data), (err) => {
+				if (err)
+					res.status(500).json({
+						status: "error",
+						message: "failed to write to file",
+					});
+				else
+					res.status(204).json({
+						status: "success",
+						message: "task deleted",
+						data: null,
+					});
+			});
+		}
+	}
 };
 
 exports.editTask = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "route not implemented",
-	});
+	const profileID = req.params.profileID * 1;
+	const profileIndex = data.findIndex((profile) => profile.id === profileID);
+	if (profileIndex === -1) {
+		res.status(404).json({
+			status: "error",
+			message: "profile not found",
+		});
+	} else {
+		const taskID = req.params.id * 1;
+		const taskIndex = data[profileIndex].tasks.findIndex(
+			(task) => task.id === taskID
+		);
+		if (taskIndex === -1) {
+			res.status(404).json({
+				status: "error",
+				message: "task not found",
+			});
+		} else {
+			if ("description" in req.body)
+				data[profileIndex].tasks[taskIndex].description =
+					req.body.description;
+			if ("status" in req.body)
+				data[profileIndex].tasks[taskIndex].status = req.body.status;
+			fs.writeFile(filepath, JSON.stringify(data), (err) => {
+				if (err)
+					res.status(500).json({
+						status: "error",
+						message: "failed to write to file",
+					});
+				else
+					res.status(203).json({
+						status: "success",
+						message: "task edited",
+						data: null,
+					});
+			});
+		}
+	}
 };
