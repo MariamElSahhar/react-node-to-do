a1f3d6afc9795a27a8e15f234501f3c15cf82274
MariamElSahhar <me1693@nyu.edu>
2024-03-26 14:13:24 +0400

    update vercel config 10

---
+++ server/api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const profileRouter = require("../routes/profileRoutes");
+const taskRouter = require("../routes/taskRoutes");
+
+const app = express();
+
+// ROUTES
+app.use(express.json());
+app.use("/api/profiles", profileRouter);
+app.use("/api/profiles", taskRouter);
+
+module.exports = app;
